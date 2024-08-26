889b7ba91c701e61aa8793f8bab31d080ae16277
Hi-Barry <128336941+Hi-Barry@users.noreply.github.com>
2024-08-26 16:00:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.barryfan.top": dockerHub,
+  "quay.barryfan.top": "https://quay.io",
+  "gcr.barryfan.top": "https://gcr.io",
+  "k8s-gcr.barryfan.top": "https://k8s.gcr.io",
+  "k8s.barryfan.top": "https://registry.k8s.io",
+  "ghcr.barryfan.top": "https://ghcr.io",
+  "cloudsmith.barryfan.top": "https://docker.cloudsmith.io",
+  "ecr.barryfan.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.barryfan.top": dockerHub,
 };
 
 function routeByHosts(host) {
